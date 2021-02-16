1512cd8827ffc616e2467f0956b7ae0fa17b6fe1
ibrahimtezvergil <74735890+ibrahimtezvergil@users.noreply.github.com>
2021-02-16 21:53:21 +0300

    completed

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -10,113 +10,38 @@ class DataController extends Controller
     protected $products="https://gorest.co.in/public-api/products";
     protected $productCategories="https://gorest.co.in/public-api/product-categories";
     protected $categories="https://gorest.co.in/public-api/categories";
+    protected $request;
+    public function __construct(RequestController $request)
+    {
+        $this->request=$request;
+    }
 
     public function index(){
         return view('welcome');
 
     }
     public function getCategories(){
-        $curl = curl_init();
+        $url=$this->categories;
+        $response=$this->request->request($url);
+        return response()->json(json_decode($response));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->categories,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_TIMEOUT => 30000,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
-                'Content-Type: application/json',
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-
-        if ($err) {
-            return response()->json($err);
-
-        } else {
-            return response()->json(json_decode($response));
-        }
     }
     public function getIndexData($page=1){
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->products."?page=".$page,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_TIMEOUT => 30000,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
-                'Content-Type: application/json',
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-
-        if ($err) {
-            return response()->json($err);
-
-        } else {
-            return response()->json(json_decode($response));
-        }
+        $url= $this->products."?page=".$page;
+        $response=$this->request->request($url);
+        return response()->json(json_decode($response));
     }
     public function getProductDetail($id=0){
-        $curl = curl_init();
+        $url=$this->products."?id=".$id;
+        $response=$this->request->request($url);
+        return response()->json(json_decode($response));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->products."?id=".$id,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_TIMEOUT => 30000,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
-                'Content-Type: application/json',
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
 
-        if ($err) {
-            return response()->json($err);
-
-        } else {
-            return response()->json(json_decode($response));
-        }
     }
     public function getCategoryData($id,$page){
-        $curl = curl_init();
+        $url=$this->products."?categories[]=".$id."&page=".$page;
+        $response=$this->request->request($url);
+        return response()->json(json_decode($response));
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->products."?categories[]=".$id."&page=".$page,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_TIMEOUT => 30000,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "GET",
-            CURLOPT_HTTPHEADER => array(
-                // Set Here Your Requesred Headers
-                'Content-Type: application/json',
-            ),
-        ));
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
-
-        if ($err) {
-            return response()->json($err);
-
-        } else {
-            return response()->json(json_decode($response));
-        }
     }
 }
