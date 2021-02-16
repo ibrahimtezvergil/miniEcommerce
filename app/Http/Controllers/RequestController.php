6a6b21c863c7c431f971d337e206b0be27fc0e8e
ibrahimtezvergil <74735890+ibrahimtezvergil@users.noreply.github.com>
2021-02-16 21:51:54 +0300

    dependency injection

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RequestController extends Controller
+{
+    public function request($url){
+                $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_TIMEOUT => 30000,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_HTTPHEADER => array(
+                // Set Here Your Requesred Headers
+                'Content-Type: application/json',
+            ),
+        ));
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+        curl_close($curl);
+
+        if ($err) {
+            return $err;
+
+        } else {
+            return $response;
+        }
+    }
+}
